49db0bb4ce8b53fce920fd0e8686f32009489196
Christian G. Warden <cwarden@xerus.org>
2013-07-18 20:50:58 -0700

    Convert debounce.js to jQuery Deferred version

--- debounce.js
+++ debounce.js
@@ -1,28 +1,31 @@
-// http://stackoverflow.com/questions/13320015/how-to-write-a-debounce-service-in-angularjs
-
-app.factory('debounce', function($timeout, $q) {
-  return function(func, wait, immediate) {
-    var timeout;
-    var deferred = $q.defer();
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if(!immediate) {
-          deferred.resolve(func.apply(context, args));
-          deferred = $q.defer();
-        }
-      };
-      var callNow = immediate && !timeout;
-      if ( timeout ) {
-        $timeout.cancel(timeout);
-      }
-      timeout = $timeout(later, wait);
-      if (callNow) {
-        deferred.resolve(func.apply(context,args));
-        deferred = $q.defer();
-      }
-      return deferred.promise;
-    };
-  };
-});
+/* Takes a function that optionally returns a promise, and debounces it, returning a
+ * promise to all callers.  When the debounced function fulfills its promise or
+ * returns a non-promise, all callers get the result.
+ * Example: http://bit.ly/1btZC4f
+ */
+var debounce = function(func, wait, immediate) {
+	var timeout;
+	var deferred = $.Deferred();
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) {
+				$.when(func.apply(context, args))
+					.then(deferred.resolve, deferred.reject, deferred.notify);
+				deferred = $.Deferred();
+			}
+		};
+		var callNow = immediate && !timeout;
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(later, wait);
+		if (callNow) {
+			$.when(func.apply(context, args))
+				.then(deferred.resolve, deferred.reject, deferred.notify);
+			deferred = $.Deferred();
+		}
+		return deferred.promise();
+	};
+};
